055c98ac88df130ba2102873f526c0d0f5321c65
Eduard Khiaev <eddietheone1984@gmail.com>
2019-05-07 13:07:25 +0300

    connect to external JSON file

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-// static JSON Content in the file
-let jsonContent = {
-    firstName: "John",
-    lastName: "Smith",
-    isAlive: true,
-    age: 27,
-    address: {
-        streetAddress: "21 2nd Street",
-        city: "New York",
-        state: "NY",
-        postalCode: "10021-3100"
-    },
-    phoneNumbers: [
-        {
-            type: "home",
-            number: "212 555-1234"
-        },
-        {
-            type: "office",
-            number: "646 555-4567"
-        },
-        {
-            type: "mobile",
-            number: "123 456-7890"
-        }
-    ],
-    children: [],
-    spouse: null
-};
-
 let output = document.getElementById("output");
 
-let phoneNumbersMap = jsonContent.phoneNumbers.map(function(phone) {
-    return `
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        // Typical action to be performed when the document is ready:
+        let jsonContent = JSON.parse(xhttp.responseText);
+        let phoneNumbersMap = jsonContent.phoneNumbers.map(function(phone) {
+            return `
           <ul class="list-group">
           <li class="p-3 mb-2 bg-info text-white">Type: ${phone.type}</li>
           <li class="p-3 mb-2 bg-success text-white">Number: ${
@@ -39,22 +14,27 @@ let phoneNumbersMap = jsonContent.phoneNumbers.map(function(phone) {
           }</li>
           </ul>
           `;
-});
+        });
 // logging for testing
 // console.log(phoneNumbersMap);
-output.innerHTML = `
+        output.innerHTML = `
 <h4 class="p-3 mb-2 bg-warning text-dark">Name: ${jsonContent.firstName} ${
-    jsonContent.lastName
-}</h4>
+            jsonContent.lastName
+        }</h4>
 <h4 class="p-3 mb-2 bg-warning text-dark">Still with us? ${
-    jsonContent.isAlive ? "Yes" : "No"
-}</h4>
+            jsonContent.isAlive ? "Yes" : "No"
+        }</h4>
 <h4 class="p-3 mb-2 bg-warning text-dark">Age: ${jsonContent.age}</h4>
 <h4 class="p-3 mb-2 bg-warning text-dark">City: ${jsonContent.address.city}</h4>
 <h4 class="p-3 mb-2 bg-warning text-dark">Street: ${
-    jsonContent.address.streetAddress
-}</h4>
+            jsonContent.address.streetAddress
+        }</h4>
 <br/>
 <h4 class="p-3 mb-2 bg-warning text-dark">Phones</h4>
 ${phoneNumbersMap.join("")}
 `;
+    }
+};
+// connect to external JSON file
+xhttp.open("GET", "json.json", true);
+xhttp.send();
